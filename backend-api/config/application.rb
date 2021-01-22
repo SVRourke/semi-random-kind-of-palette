21e880aba1e06d3f3acc51b24c9d7d848330d285
SVRourke <samuel@rourke.tech>
2021-01-22 10:25:22 -0500

    removed unused default routes

--- backend-api/config/application.rb
+++ backend-api/config/application.rb
@@ -43,5 +43,10 @@ module BackendApi
         )
       end
     end
+
+    initializer(:remove_action_mailbox_and_activestorage_routes, after: :add_routing_paths) { |app|
+      app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}
+      app.routes_reloader.paths.delete_if {|path| path =~ /actionmailbox/ }
+    }
   end
 end
