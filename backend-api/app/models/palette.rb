4e9b610cc43ff844aa77bae205148feddddf66e0
SVRourke <samuel@rourke.tech>
2021-01-21 23:37:48 -0500

    added methods for creating a random palette and creating a new palette from a hash of values

--- backend-api/app/models/palette.rb
+++ backend-api/app/models/palette.rb
@@ -1,3 +1,21 @@
 class Palette < ApplicationRecord
     has_and_belongs_to_many :colors
+
+    def self.newRandom()
+        palette = Palette.new(name: "palette-#{("a".."z").to_a.concat((0..9).to_a).shuffle.slice(0,4).join("")}")
+        palette.colors.concat(Color.all.sample(5))
+        palette.save()
+        return palette
+    end
+
+    def self.new_from_hash(info)
+        newPalette = Palette.new(name: info[:name])
+
+        info[:color_ids].each do |cid|
+            newPalette.colors << Color.find(cid)
+        end
+
+        newPalette.save()
+        return newPalette
+    end
 end
