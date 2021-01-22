04b4aae4306c44568f19f74df02141afdb79bded
SVRourke <samuel@rourke.tech>
2021-01-21 23:27:19 -0500

    fixed typo

--- backend-api/db/migrate/20210122040713_create_colors_palettes.rb
+++ backend-api/db/migrate/20210122040713_create_colors_palettes.rb
@@ -1,6 +1,6 @@
 class CreateColorsPalettes < ActiveRecord::Migration[6.0]
   def change
-    create__join_table :colors, :palettes do |t|
+    create_join_table :colors, :palettes do |t|
       t.index :color_id
       t.index :palette_id
     end
