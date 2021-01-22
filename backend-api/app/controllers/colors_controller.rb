a2e107242c5f56f8e9c213d2f3150dbf809f708c
SVRourke <samuel@rourke.tech>
2021-01-22 09:15:48 -0500

    action to render the id, name and hex value of n randomly chosen colors

--- backend-api/app/controllers/colors_controller.rb
+++ backend-api/app/controllers/colors_controller.rb
@@ -4,4 +4,9 @@ class ColorsController < ApplicationController
         render json: colors, only: [:name, :hex]
     end
 
+    def nRandom
+        colors = Color.all.sample(params[:n].to_i)
+        render json: colors, only: [:id, :name, :hex]
+    end
+
 end
