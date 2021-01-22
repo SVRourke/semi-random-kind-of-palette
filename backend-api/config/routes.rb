e7ecd71cd0e7a2b45e143eebe359320b592f0b68
SVRourke <samuel@rourke.tech>
2021-01-22 10:15:14 -0500

    refactored

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  
-  # Route for all colors
+  # Route for all colors or n random 
+  # colors if a "count" query string is suplied
+  # /api/colors?count=7 would return 7 random colors
   get '/api/colors', to: 'colors#index'
-  # Route for N random colors
-  get '/api/:n/colors', to: 'colors#nRandom'
-
   # Route for all palettes
   get '/api/palettes', to: 'palettes#index'
-
   # Route to create a new palette
   post '/api/palettes', to: 'palettes#create'
-
-
 end
