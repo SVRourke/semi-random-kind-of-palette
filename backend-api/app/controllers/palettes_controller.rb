d5dbec7c498a02fe707e763ca6a107603e059516
SVRourke <samuel@rourke.tech>
2021-01-22 09:51:35 -0500

    fixed rendering

--- backend-api/app/controllers/palettes_controller.rb
+++ backend-api/app/controllers/palettes_controller.rb
@@ -1,10 +1,10 @@
 class PalettesController < ApplicationController
     def index
         palettes = Palette.all
-        render :json => palettes,
+        render :json => palettes, 
             :include => {
-                :only => [:id, :name, :hex]
-            },
+                :colors => {:only => [:id, :name, :hex]}
+            }, 
             :except => [:created_at, :updated_at]
     end
 
