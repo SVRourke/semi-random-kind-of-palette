1e80aead62c95bce278bae7516a9ef55f558cbad
SVRourke <samuel@rourke.tech>
2021-01-21 23:09:58 -0500

    created color migration

--- backend-api/db/migrate/20210122040320_create_colors.rb
+++ backend-api/db/migrate/20210122040320_create_colors.rb
@@ -1,7 +1,15 @@
 class CreateColors < ActiveRecord::Migration[6.0]
   def change
     create_table :colors do |t|
-
+      t.string :name
+      t.string :hex
+      t.string :red
+      t.string :green
+      t.string :blue
+      t.string :hue
+      t.string :saturation
+      t.string :light
+      t.string :value
       t.timestamps
     end
   end
