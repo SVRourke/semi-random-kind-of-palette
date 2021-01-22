a5f4361541c87947101f4833cc71fb86fb2d5ef1
SVRourke <samuel@rourke.tech>
2021-01-22 09:19:24 -0500

    route for n random colors

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # Route for all colors
   GET '/api/colors', to: 'colors#index'
   
+  # Route for N random colors
+  GET '/api/:n/colors', to: 'colors#nRandom'
 
 
 end
