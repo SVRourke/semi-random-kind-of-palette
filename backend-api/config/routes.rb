a80d1faf3aa60409f6509b896c0c66f1cc61d186
SVRourke <samuel@rourke.tech>
2021-01-22 09:20:22 -0500

    route for palettes index

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   
   # Route for all colors
   GET '/api/colors', to: 'colors#index'
-  
   # Route for N random colors
   GET '/api/:n/colors', to: 'colors#nRandom'
+
+  # Route for all palettes
+  GET '/api/palettes', to: 'palettes#index'
+  
 
 
 end
