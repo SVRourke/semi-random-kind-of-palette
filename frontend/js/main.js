7fc3462cf7ecfd668f8ad45ebdf97f3c756f465d
SVRourke <samuel@rourke.tech>
2021-01-24 16:19:41 -0500

    callback to toggle locked icon, function to create palette color column, for loop to generate random new template on load

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -7,6 +7,20 @@ function newElem(tag, names) {
     return e
 }
 
+// Callback for event listener on main palette color lock
+// toggles the locked/unlocked icon and color column's data-color-unlocked attribute
+function toggleIcon(icon) {
+    if (icon.classList.contains("fa-unlock")) {
+        icon.classList.remove("fa-unlock")
+        icon.classList.add("fa-lock")
+        icon.parentElement.setAttribute("data-color-unlocked", false);
+    } else {
+        icon.classList.add("fa-unlock")
+        icon.classList.remove("fa-lock")
+        icon.parentElement.setAttribute("data-color-unlocked", true);
+    }
+}
+
 function miniPalette(info) {
     let card = newElem("div", ["mini-card"]);
     let row = newElem("div", ["row"]);
@@ -27,21 +41,23 @@ function miniPalette(info) {
 }
 
 
+// Creates and returns a column element for the main palette
+function createColumn(color) {
+    let div = newElem("div", ["color"]);
+    div.setAttribute("data-color_id", color.id);
+    div.setAttribute("data-color-unlocked", true);
+    div.style.backgroundColor = color.hex;
+    
+    let icon = newElem("i", ["fa", "fa-unlock"])
+    icon.addEventListener("click", (e) => {toggleIcon(e.target)})
+    div.appendChild(icon);
+    return div
+}
+
+
+// mainColorsContainer.appendChild(createColumn({id: 76, name: "blah", hex: "#655044"}))
+
 let mainColorsContainer = document.querySelector(".palette_row");
-
-
-// <div class="color" id="four">
-//     <i class="fa fa-lock"></i>
-// </div>
-
-// <div class="color" id="five">
-//     <i class="fa fa-unlock"></i>
-// </div>
-
-
-
-
-
 
 document.addEventListener("DOMContentLoaded", () => {
     container = document.querySelector("section.palettes")
@@ -51,6 +67,14 @@ document.addEventListener("DOMContentLoaded", () => {
         for (const o of d) {container.appendChild(miniPalette(o))}
     })
     .catch(e => console.log(e));
+
+    for (let i = 0; i < 5; i++) {
+        fetch("http://localhost:3000/api/colors?count=1")
+        .then(r => r.json())
+        .then((d) => {
+            mainColorsContainer.appendChild(createColumn(d[0]))
+        })
+    }
 });
 
 
