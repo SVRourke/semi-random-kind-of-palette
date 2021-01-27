6a60fd61f7bb3486d5675c7f50acb8d1953a00bc
SVRourke <samuelvanrourke@gmail.com>
2021-01-27 08:14:32 -0500

    fixed toggle icon

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -21,9 +21,8 @@ function toggleIcon(icon) {
     if (icon.classList.contains("fa-unlock")) {
         icon.classList.replace("fa-unlock", "fa-lock")
         icon.parentElement.setAttribute("data-color-unlocked", false);
-        icon.setAttribute("alt", "locked")
     } else {
-        icon.classList.replace("fa-unlock", "fa-lock")
+        icon.classList.replace("fa-lock", "fa-unlock")
         icon.parentElement.setAttribute("data-color-unlocked", true);
     }
 }
